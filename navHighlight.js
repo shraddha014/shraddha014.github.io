d2e9b803f25997d369a50d6adba9452fd2006ed3
Shraddha <ms.shraddha14@gmail.com>
2024-08-18 19:25:05 -0500

    added content for education, experience and project section

---
+++ navHighlight.js
@@ -0,0 +1,19 @@
+// Get the current page's URL
+const currentPage = window.location.pathname.split("/").pop();
+console.log("currentpage", currentPage);
+
+// Map the paths to their corresponding link IDs
+const navLinks = {
+  "index.html": "home-id",
+  "experience.html": "experience-id",
+  "education.html": "education-id",
+  "projects.html": "projects-id",
+};
+
+// Get the ID of the link to activate
+const activeLinkId = navLinks[currentPage];
+
+// Add the 'active' class to the corresponding link
+if (activeLinkId) {
+  document.getElementById(activeLinkId).classList.add("active");
+}
